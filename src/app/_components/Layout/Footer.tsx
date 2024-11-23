2d4590b8e8909c297736f72ef92e3aa4442ccb65
Facundo Marcó del Pont <facundo.marcodelpont@replypro.io>
2024-11-23 09:45:30 -0300

    add social media

--- src/app/_components/Layout/Footer.tsx
+++ src/app/_components/Layout/Footer.tsx
@@ -1,12 +1,30 @@
+import XLogo from '@/components/icons/XLogo'
+import { MailIcon } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
-const ISOLOGO_SIZE = 40
+const ISOLOGO_SIZE = 65
 
 const Footer = () => {
   return (
-    <footer className='w-full bg-green-950 flex justify-center items-center h-16 flex-shrink-0'>
-      <div className='max-w-[600px] flex justify-center items-center mt-1'>
+    <footer className='w-full bg-green-950 flex justify-center items-center flex-shrink-0 py-2 px-3'>
+      <div className='max-w-[600px] flex justify-center items-center mt-1 relative flex-1'>
         <Image src='/Isologo.png' alt='Splitify' width={ISOLOGO_SIZE} height={ISOLOGO_SIZE} />
+        <div className='flex gap-1 absolute right-0 bottom-2'>
+          <Link
+            href='https://x.com/splitify_me'
+            target='_blank'
+            className='rounded-full p-1 size-5 flex items-center justify-center text-white'
+          >
+            <XLogo />
+          </Link>
+          <Link
+            href='mailto:splitify.me@gmail.com'
+            className='rounded-full p-1 size-5 flex items-center justify-center text-white'
+          >
+            <MailIcon />
+          </Link>
+        </div>
       </div>
     </footer>
   )
