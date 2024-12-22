3eb1cd202479e1db7c0547b212af241f1dfbeeb2
Facundo Marcó del Pont <facundo.marcodelpont@replypro.io>
2024-12-22 18:24:42 -0300

    make a single function

--- src/lib/functions/calculateBalances.ts
+++ src/lib/functions/calculateBalances.ts
@@ -1,6 +1,11 @@
 import { Expense } from '@/types/Expense'
 import { Balance } from '@/types/Balance'
 
+// SALÍ DE ACÁ PÁ
+// SALÍ DE ACÁ PÁ
+// SALÍ DE ACÁ PÁ
+// SALÍ DE ACÁ PÁ
+// SALÍ DE ACÁ PÁ
 export function calculateBalances(expenses: Array<Pick<Expense, 'name' | 'amount'>>): Balance[] {
   // Calculate the total sum and per-person spending
   const total = expenses.reduce((sum, { amount }) => sum + amount, 0)
