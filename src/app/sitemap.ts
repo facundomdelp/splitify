39656232d85aea2e236e8eb7a3ffad7bcd7e987d
Facundo Marcó del Pont <facundo.marcodelpont@replypro.io>
2025-01-03 17:37:36 -0300

    remove seo routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,3 @@
-import { SEO_ROUTES } from '@/seo/seoRoutes'
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
@@ -6,13 +5,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   // General routes that exist for all locales
   const commonRoutes = ['/']
-
-  // Locale-specific routes
-  const localeRoutes = {
-    en: Object.keys(SEO_ROUTES.en).map((route) => `/${route}`),
-    es: Object.keys(SEO_ROUTES.es).map((route) => `/${route}`),
-    pt: Object.keys(SEO_ROUTES.pt).map((route) => `/${route}`),
-  }
 
   const sitemapEntries: MetadataRoute.Sitemap = locales.flatMap((locale) => [
     // Common routes for all locales
@@ -27,26 +19,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
           href: `https://splitify.me/${altLocale}${route === '/' ? '' : route}`,
           hreflang: altLocale,
         })),
-    })),
-
-    // Locale-specific routes
-    ...localeRoutes[locale as keyof typeof localeRoutes].map((route) => ({
-      url: `https://splitify.me/${locale}${route}`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-      alternateRefs: locales
-        .filter((altLocale) => altLocale !== locale)
-        .map((altLocale) => {
-          // Try to find an equivalent route in other locales
-          const altRoute = localeRoutes[altLocale as keyof typeof localeRoutes].find(
-            (r) => r.replace(/^\//, '') === route.replace(/^\//, ''),
-          )
-          return {
-            href: `https://splitify.me/${altLocale}${altRoute || ''}`,
-            hreflang: altLocale,
-          }
-        }),
     })),
   ]) as MetadataRoute.Sitemap
 
