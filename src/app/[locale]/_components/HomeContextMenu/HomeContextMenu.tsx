10f5ded0fdcc16a0a2cfe3afcefbdbfc130b30f2
Facundo Marcó del Pont <facundo.marco@marvik.ai>
2025-01-20 19:38:13 -0300

    beta badge for turn into a group feature

--- src/app/[locale]/_components/HomeContextMenu/HomeContextMenu.tsx
+++ src/app/[locale]/_components/HomeContextMenu/HomeContextMenu.tsx
@@ -1,6 +1,7 @@
 import ConfirmationModal from '@/components/ConfirmationModal'
 import ContextMenu from '@/components/ContextMenu'
 import { ContextMenuItem } from '@/components/ContextMenu/ContextMenu'
+import { Badge } from '@/components/ui/badge'
 import { Balance } from '@/types/balance.types'
 import { Expense } from '@/types/expense.types'
 import { Eraser, Wand } from 'lucide-react'
@@ -45,6 +46,9 @@ const HomeContextMenu = ({ expenses, setExpenses, setBalances, convertIntoGroup 
       <ContextMenu>
         <ContextMenuItem onClick={() => setOpenTurnIntoGroupConfirmationModal(true)} disabled={expenses.length === 0}>
           <Wand /> Turn into a Group ✈️
+          <Badge className='my-auto px-[6px] uppercase text-nowrap opacity-70 rounded-lg text-[6px] flex leading-[0.6rem] pointer-events-none'>
+            Beta
+          </Badge>
         </ContextMenuItem>
         <ContextMenuItem onClick={() => setOpenResetConfirmationModal(true)} disabled={expenses.length === 0}>
           <Eraser /> Reset Expenses
