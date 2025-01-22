57cb86b192ceb22bf9c413b6d89c72bc256c8148
Facundo Marcó del Pont <facundo.marco@marvik.ai>
2025-01-22 20:34:56 -0300

    fix issue with tabs height

--- src/components/ExpensesBalancesTabs/ExpensesBalancesTabs.tsx
+++ src/components/ExpensesBalancesTabs/ExpensesBalancesTabs.tsx
@@ -25,7 +25,7 @@ const ExpensesBalancesTabs = ({ children, onBalancesClick, disabled, disabledExp
     <Tabs
       value={tabValue}
       onValueChange={(value) => setTabValue(value as 'expenses' | 'balances')}
-      className='w-full flex flex-col h-full'
+      className='w-full flex flex-col flex-1'
     >
       <div className='flex relative'>
         <TabsList className='mx-auto scale-90 xs:scale-100'>
@@ -43,13 +43,14 @@ const ExpensesBalancesTabs = ({ children, onBalancesClick, disabled, disabledExp
         </TabsList>
       </div>
 
-      <TabsContent className='mt-4 flex-1 flex flex-col gap-4' value='expenses'>
-        {children[0]}
-
-        <CalculateButton onClick={handleCalculateButton} disabled={disabled || disabledBalances} />
+      <TabsContent className='flex-1' value='expenses'>
+        <div className='mt-2 h-full flex flex-col gap-4'>
+          {children[0]}
+          <CalculateButton onClick={handleCalculateButton} disabled={disabled || disabledBalances} />
+        </div>
       </TabsContent>
-      <TabsContent className='mt-8 flex-1 flex flex-col' value='balances'>
-        {children[1]}
+      <TabsContent className='flex-1' value='balances'>
+        <div className='mt-2 h-full flex-1 flex flex-col gap-4'>{children[1]}</div>
       </TabsContent>
     </Tabs>
   )
