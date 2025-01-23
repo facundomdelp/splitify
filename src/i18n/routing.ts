73d9bc12b09998235db05a8b553eee67fb546036
Facundo Marcó del Pont <facundo.marco@marvik.ai>
2025-01-23 10:02:36 -0300

    remove /en/

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,7 @@ import { defineRouting } from 'next-intl/routing'
 export const routing = defineRouting({
   locales: ['en', 'es', 'pt'],
   defaultLocale: 'en',
+  localePrefix: 'as-needed',
 })
 
 // Lightweight wrappers around Next.js' navigation APIs
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ import { routing } from './i18n/routing'
 export default createMiddleware(routing)
 
 export const config = {
-  matcher: ['/', '/(en|es|pt)/:path*'],
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
 }
