2eda617273beccedba11df4a7c87a4b1f92e395f
Facundo Marcó del Pont <facundomdelp@gmail.com>
2026-02-26 19:53:25 -0300

    fix

--- src/utils/functions/generateRandomName.ts
+++ src/utils/functions/generateRandomName.ts
@@ -5,12 +5,13 @@ import { ADJECTIVES, NOUNS } from '../constants/names'
 export const generateRandomName = (locale?: Locale) => {
   locale = locale || 'en'
 
-  const adjective = ADJECTIVES[locale as keyof typeof ADJECTIVES]
-  const nouns = NOUNS[locale as keyof typeof NOUNS]
+  const adjective = ADJECTIVES[locale]
+  const nouns = NOUNS[locale]
 
   const randomAdjective = adjective[Math.floor(Math.random() * adjective.length)]
   const randomNoun = nouns[Math.floor(Math.random() * nouns.length)]
 
-  if (locale === 'en') return `${randomAdjective} ${randomNoun}`
+  const adjectiveFirstLocales: Locale[] = ['en', 'de', 'ja', 'ru', 'zh-CN', 'zh-TW']
+  if (adjectiveFirstLocales.includes(locale)) return `${randomAdjective} ${randomNoun}`
   return `${randomNoun} ${randomAdjective}`
 }
